780ea888e8d73f69c680cbc7ad84d054da51f153
junaidalianwar23 <junaidalianwar23@example.com>
2024-04-15 15:24:59 +0530

    project added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Todo
+# Title: To-Do List
+Technology used:
+`HTML`
+`CSS`
+`JavaScript`
+
+Description:
+This is a to-do web app where users can:
+- Write their daily tasks
+- Mark tasks as completed
+- Delete tasks when needed
+
---
+++ web/logic.js
@@ -0,0 +1,32 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+const inputBox = document.getElementById("task-title");
+const listContainer = document.getElementById("task-list");
+
+function addTask() {
+
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
